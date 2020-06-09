d8b4ef6451a467c03b6e1799d2059255609b85f9
NiharRan <niharranjandasmu@gmail.com>
2020-06-09 23:22:57 +0600

    database sql

--- application/db/restaurant.sql
+++ application/db/restaurant.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 20, 2020 at 09:36 AM
--- Server version: 10.4.8-MariaDB
--- PHP Version: 7.3.11
+-- Generation Time: Jun 09, 2020 at 07:20 PM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 5.6.40
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,6 +25,37 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `booking_requests`
+--
+
+CREATE TABLE `booking_requests` (
+  `request_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(14) NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL,
+  `people` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `request_checked` tinyint(1) NOT NULL DEFAULT '0',
+  `request_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `request_confirmed_at` datetime NOT NULL,
+  `request_confirmed_by` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `booking_requests`
+--
+
+INSERT INTO `booking_requests` (`request_id`, `name`, `email`, `phone`, `date`, `time`, `people`, `message`, `request_checked`, `request_date`, `request_confirmed_at`, `request_confirmed_by`) VALUES
+(1, 'Nihar Ranjan Das', 'niharranjandasmu@gmail.com', '01761152186', '2020-01-27', '15:30:00', 30, 'Proper Arangement', 1, '2020-01-26 15:57:43', '0000-00-00 00:00:00', 0),
+(2, 'Akash Das', 'akash@gmail.com', '01761442186', '2020-02-02', '10:30:00', 15, 'I want to book tables for 4 hours', 1, '2020-02-02 08:07:50', '0000-00-00 00:00:00', 0),
+(3, 'Nihar Ranjan Das', 'niharranjandasmu@gmail.com', '01761152186', '2020-02-05', '16:00:00', 30, 'for birthday party. for 3 hours', 1, '2020-02-05 11:46:48', '0000-00-00 00:00:00', 0),
+(4, 'Nihar Ranjan Das', '', '01761152186', '2020-02-13', '22:30:00', 30, 'ggfjgf fghjtgfhj', 0, '2020-02-05 14:23:47', '0000-00-00 00:00:00', 0);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `category`
 --
 
@@ -39,7 +70,9 @@ CREATE TABLE `category` (
 --
 
 INSERT INTO `category` (`id`, `name`, `active`) VALUES
-(1, 'Biriyani', 1);
+(1, 'Biriyani', 1),
+(2, 'Gril', 1),
+(3, 'Burger', 1);
 
 -- --------------------------------------------------------
 
@@ -56,15 +89,17 @@ CREATE TABLE `company` (
   `phone` varchar(255) NOT NULL,
   `country` varchar(255) NOT NULL,
   `message` text NOT NULL,
-  `currency` varchar(255) NOT NULL
+  `currency` varchar(255) NOT NULL,
+  `close_at` date DEFAULT NULL,
+  `open_at` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `company`
 --
 
-INSERT INTO `company` (`id`, `company_name`, `service_charge_value`, `vat_charge_value`, `address`, `phone`, `country`, `message`, `currency`) VALUES
-(1, 'Lulu', '', '13', 'READING', '234234235', 'Spai', 'this is just an testing', 'USD');
+INSERT INTO `company` (`id`, `company_name`, `service_charge_value`, `vat_charge_value`, `address`, `phone`, `country`, `message`, `currency`, `close_at`, `open_at`) VALUES
+(1, 'Restaurant-Kit', '5.1', '13', 'Sylhet', '01761152441', 'Bangladesh', 'It\'s the place you love to eat, book now!', 'USD', '2020-06-08', '2020-06-08');
 
 -- --------------------------------------------------------
 
@@ -84,8 +119,8 @@ CREATE TABLE `groups` (
 
 INSERT INTO `groups` (`id`, `group_name`, `permission`) VALUES
 (1, 'Super Administrator', 'a:32:{i:0;s:10:\"createUser\";i:1;s:10:\"updateUser\";i:2;s:8:\"viewUser\";i:3;s:10:\"deleteUser\";i:4;s:11:\"createGroup\";i:5;s:11:\"updateGroup\";i:6;s:9:\"viewGroup\";i:7;s:11:\"deleteGroup\";i:8;s:11:\"createStore\";i:9;s:11:\"updateStore\";i:10;s:9:\"viewStore\";i:11;s:11:\"deleteStore\";i:12;s:11:\"createTable\";i:13;s:11:\"updateTable\";i:14;s:9:\"viewTable\";i:15;s:11:\"deleteTable\";i:16;s:14:\"createCategory\";i:17;s:14:\"updateCategory\";i:18;s:12:\"viewCategory\";i:19;s:14:\"deleteCategory\";i:20;s:13:\"createProduct\";i:21;s:13:\"updateProduct\";i:22;s:11:\"viewProduct\";i:23;s:13:\"deleteProduct\";i:24;s:11:\"createOrder\";i:25;s:11:\"updateOrder\";i:26;s:9:\"viewOrder\";i:27;s:11:\"deleteOrder\";i:28;s:10:\"viewReport\";i:29;s:13:\"updateCompany\";i:30;s:11:\"viewProfile\";i:31;s:13:\"updateSetting\";}'),
-(4, 'Members', 'a:9:{i:0;s:9:\"viewStore\";i:1;s:11:\"deleteStore\";i:2;s:9:\"viewTable\";i:3;s:11:\"deleteTable\";i:4;s:12:\"viewCategory\";i:5;s:11:\"viewProduct\";i:6;s:11:\"createOrder\";i:7;s:11:\"updateOrder\";i:8;s:9:\"viewOrder\";}'),
-(5, 'Staff', 'a:6:{i:0;s:9:\"viewTable\";i:1;s:11:\"viewProduct\";i:2;s:11:\"createOrder\";i:3;s:11:\"updateOrder\";i:4;s:9:\"viewOrder\";i:5;s:11:\"viewProfile\";}');
+(4, 'Members', 'a:12:{i:0;s:9:\"viewStore\";i:1;s:11:\"deleteStore\";i:2;s:9:\"viewTable\";i:3;s:11:\"deleteTable\";i:4;s:13:\"updateRequest\";i:5;s:11:\"viewRequest\";i:6;s:13:\"deleteRequest\";i:7;s:12:\"viewCategory\";i:8;s:11:\"viewProduct\";i:9;s:11:\"createOrder\";i:10;s:11:\"updateOrder\";i:11;s:9:\"viewOrder\";}'),
+(5, 'Staff', 'a:9:{i:0;s:9:\"viewTable\";i:1;s:13:\"updateRequest\";i:2;s:11:\"viewRequest\";i:3;s:13:\"deleteRequest\";i:4;s:11:\"viewProduct\";i:5;s:11:\"createOrder\";i:6;s:11:\"updateOrder\";i:7;s:9:\"viewOrder\";i:8;s:11:\"viewProfile\";}');
 
 -- --------------------------------------------------------
 
@@ -110,6 +145,16 @@ CREATE TABLE `orders` (
   `store_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `bill_no`, `date_time`, `gross_amount`, `service_charge_rate`, `service_charge_amount`, `vat_charge_rate`, `vat_charge_amount`, `discount`, `net_amount`, `user_id`, `table_id`, `paid_status`, `store_id`) VALUES
+(1, 'BILPR-8404', '1579966210', '320.00', '', '0', '13', '41.60', '', '361.60', 6, 1, 1, 1),
+(2, 'BILPR-1163', '1580049533', '3840.00', '', '0', '13', '499.20', '500', '3839.20', 1, 2, 1, 1),
+(3, 'BILPR-DAE9', '1580912754', '19.70', '5.1', '1.00', '13', '2.56', '', '23.26', 1, 1, 1, 0),
+(4, 'BILPR-DB80', '1580913402', '320.00', '5.1', '16.32', '13', '41.60', '', '377.92', 1, 2, 1, 0);
+
 -- --------------------------------------------------------
 
 --
@@ -124,6 +169,16 @@ CREATE TABLE `order_items` (
   `rate` varchar(255) NOT NULL,
   `amount` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `order_items`
+--
+
+INSERT INTO `order_items` (`id`, `order_id`, `product_id`, `qty`, `rate`, `amount`) VALUES
+(4, 1, 1, '1', '320', '320.00'),
+(5, 2, 1, '12', '320', '3840.00'),
+(7, 3, 2, '1', '19.7', '19.70'),
+(9, 4, 1, '1', '320', '320.00');
 
 -- --------------------------------------------------------
 
@@ -141,6 +196,14 @@ CREATE TABLE `products` (
   `image` text NOT NULL,
   `active` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `category_id`, `store_id`, `name`, `price`, `description`, `image`, `active`) VALUES
+(1, '[\"2\"]', '[\"1\"]', 'Chicken Gril', '320', '<p>Full Chicken Gril with sauce</p>', 'assets/images/product_image/5e26c219a4549.jpg', 1),
+(2, '[\"3\"]', '[\"1\"]', 'Burger', '19.7', '', 'assets/images/product_image/5e3681f697fce.jpg', 1);
 
 -- --------------------------------------------------------
 
@@ -181,7 +244,38 @@ CREATE TABLE `tables` (
 --
 
 INSERT INTO `tables` (`id`, `table_name`, `capacity`, `available`, `active`, `store_id`) VALUES
-(1, 'North', '10', 1, 1, 1);
+(1, 'Right-002', '10', 1, 1, 1),
+(2, 'Right-001', '10', 1, 1, 1),
+(3, 'Left-001', '10', 2, 1, 1),
+(4, 'Left-002', '10', 2, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `table_bookings`
+--
+
+CREATE TABLE `table_bookings` (
+  `booking_id` int(11) NOT NULL,
+  `table_id` int(11) NOT NULL,
+  `booking_start` datetime NOT NULL,
+  `booking_end` datetime NOT NULL,
+  `booking_status` tinyint(1) NOT NULL DEFAULT '0',
+  `booking_doc` datetime NOT NULL,
+  `booking_dom` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `table_bookings`
+--
+
+INSERT INTO `table_bookings` (`booking_id`, `table_id`, `booking_start`, `booking_end`, `booking_status`, `booking_doc`, `booking_dom`) VALUES
+(1, 2, '2020-01-27 15:00:00', '2020-01-27 16:30:00', 1, '2020-01-26 20:16:35', '2020-01-26 19:16:35'),
+(2, 4, '2020-02-05 11:00:00', '2020-02-05 20:00:00', 1, '2020-02-05 12:02:23', '2020-02-05 11:02:23'),
+(3, 3, '2020-02-05 21:00:00', '2020-02-05 23:00:00', 1, '2020-02-05 15:05:25', '2020-02-05 14:05:25'),
+(4, 3, '2020-02-05 17:00:00', '2020-02-05 20:00:00', 1, '2020-02-05 15:07:30', '2020-02-05 14:07:30'),
+(5, 1, '2020-02-05 20:00:00', '2020-02-05 20:00:00', 1, '2020-02-05 15:09:25', '2020-02-05 14:09:25'),
+(6, 2, '2020-02-05 20:00:00', '2020-02-05 20:00:00', 1, '2020-02-05 15:09:25', '2020-02-05 14:09:25');
 
 -- --------------------------------------------------------
 
@@ -206,8 +300,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `email`, `firstname`, `lastname`, `phone`, `gender`, `store_id`) VALUES
-(1, 'admin', '$2y$10$yfi5nUQGXUZtMdl27dWAyOd/jMOmATBpiUvJDmUu9hJ5Ro6BE5wsK', 'admin@admin.com', 'john', 'doe', '80789998', 1, 0),
-(6, 'kalam', '$2y$10$/hEXxIbvW321dTwK7MQnWuClJVywK2qN1WvrzdKZXG1jOVOQkN9Va', 'kalamkajnai@gmail.com', 'Kalam', 'Lal', '018273635635', 1, 1);
+(1, 'admin', '$2a$08$V75yjCpLGm8qKFvI/vPfm.LqYCdxmiV1X0YYHrGTonV.ytZLw01Tm', 'demo@admin.com', 'john', 'doe', '80789998', 1, 0),
+(6, 'kalam', '$2a$08$V75yjCpLGm8qKFvI/vPfm.LqYCdxmiV1X0YYHrGTonV.ytZLw01Tm', 'kalamkajnai@gmail.com', 'Kalam', 'Lal', '018273635635', 1, 1),
+(7, 'nihar007', '$2y$10$TFsWvE50gR5FbRDvsJABFusYWq/Qun.kFYDhpQBcGaiqP/4vWFlea', 'demo@demo.com', 'Nihar', 'Das', '01761152186', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -227,11 +322,18 @@ CREATE TABLE `user_group` (
 
 INSERT INTO `user_group` (`id`, `user_id`, `group_id`) VALUES
 (1, 1, 1),
-(7, 6, 5);
+(7, 6, 5),
+(8, 7, 4);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `booking_requests`
+--
+ALTER TABLE `booking_requests`
+  ADD PRIMARY KEY (`request_id`);
 
 --
 -- Indexes for table `category`
@@ -282,6 +384,12 @@ ALTER TABLE `tables`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `table_bookings`
+--
+ALTER TABLE `table_bookings`
+  ADD PRIMARY KEY (`booking_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -298,10 +406,16 @@ ALTER TABLE `user_group`
 --
 
 --
+-- AUTO_INCREMENT for table `booking_requests`
+--
+ALTER TABLE `booking_requests`
+  MODIFY `request_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `company`
@@ -319,19 +433,19 @@ ALTER TABLE `groups`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `order_items`
 --
 ALTER TABLE `order_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `stores`
@@ -343,19 +457,25 @@ ALTER TABLE `stores`
 -- AUTO_INCREMENT for table `tables`
 --
 ALTER TABLE `tables`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `table_bookings`
+--
+ALTER TABLE `table_bookings`
+  MODIFY `booking_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `user_group`
 --
 ALTER TABLE `user_group`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
